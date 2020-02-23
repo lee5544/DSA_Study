0c5dfe2627f1c6cbb204314d76198542faa587b1
李勇 <44977031+lee5544@users.noreply.github.com>
2020-02-23 21:15:40 +0800

    vector

--- Vector/Vector/Vector/Vector.cpp
+++ Vector/Vector/Vector/Vector.cpp
@@ -1,1 +1,11 @@
 ﻿
+#include "Vector.h"
+
+template <typename T>
+
+void Vector<T>::copyFrom(T const* A, Rank lo, Rank hi)
+{
+	_elem = new T[2 * (hi - lo + 1)];
+	_size = 0;
+	while (lo < hi){ _elem[_size++] = A[lo++]; }
+}
--- Vector/Vector/Vector/Vector.h
+++ Vector/Vector/Vector/Vector.h
@@ -1,1 +1,31 @@
 #pragma once
+/*
+*	向量数据结构
+*/
+
+typedef int Rank;	//秩
+#define DEFAULT_CAPACITY 10;	//向量的默认容量
+
+template <typename T> class Vector {
+protected:
+	Rank _size; int capacity; T* _elem;    //规模，容量，数据区
+	void copyFrom(T const* A, Rank lo, Rank hi);
+
+public:
+	//构造函数
+	Vector(int c = DEFAULT_CAPACITY, int s, T v = 0)	//默认构造函数
+	{
+		_elem = new T[_capacity = c];
+		for (_size = 0; _size < s; _elem[_size++] = v);
+	};
+	Vector(T const* A, Rank lo, Rank hi) { copyFrom(A, lo, hi); };
+	Vector(T const* A, Rank n) { copyFrom(A, 0, n); };
+	Vector(Vector<T> const& V, Rank lo, Rank hi) { copyFrom(V._elem, lo, hi); };
+	Vector(Vector<T> const& V) { copyFrom(V._elem, 0, V._size); };
+	//析构函数
+	~Vector() { delete[] _elem; };
+	//只读访问接口
+	//可写访问接口
+	//遍历
+
+};
